99e9a3370ced593d34ba152c758cc02c889a5e1f
venu <svsvenu@gmail.com>
2017-09-06 18:44:03 +0000

    removed files

--- drivers/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/kernel_modules/drivers/built-in.o :=  rm -f /home/pi/kernel_modules/drivers/built-in.o; ar rcsD /home/pi/kernel_modules/drivers/built-in.o
--- drivers/.tmp_versions/list_tasks.mod
+++
@@ -1,3 +0,0 @@
-/home/pi/kernel_modules/drivers/list_tasks.ko
-/home/pi/kernel_modules/drivers/list_tasks.o
-
